40c28f159d5487c74243186b2a7f2d1fcbb7d7e3
mankind4dev <ogunmakin120@gmail.com>
2024-12-24 08:07:38 +0100

    profile edit

--- client/src/components/dashCompo/Profile.jsx
+++ client/src/components/dashCompo/Profile.jsx
@@ -10,7 +10,8 @@ export default function Profile() {
       <DashboardHome>
         <div className="flex flex-col w-full">
           <div className="border-b-2 py-2 w-full">
-            <h1 className="text-[50px] capitalize">profile</h1>
+            <h1 className="text-[50px] capitalize">profile setting</h1>
+            <p className="text-[12px] text-slate-500">Update your profile and upload your image 👍🏻</p>
           </div>
           <ProfileForm />
         </div>
